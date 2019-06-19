a758ed7e5a5ec97b0b2056d0b272a052ca68c155
Viraj Patel <Viraj84@gmail.com>
2019-06-19 19:35:39 -0400

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ function App() {
   return (
     <div className="App">
       <h1>Mars Photo API</h1>
+      <h3>Curiosity Rover</h3>
+      <h5>Search by Sol day number and camera</h5>
       <SearchView/>     
     </div>
   );
